8b0fb7e393d4c914536ee24e2dadcef78c94e598
Abhishek Patil <abhishekp.7841@gmail.com>
2025-05-16 17:46:22 +0530

    Update Location.js

--- models/Location.js
+++ models/Location.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
   socketId: { type: String, required: true, unique: true },
-  latitude: Number,
-  longitude: Number,
-  accuracy: Number,
+  currentLocation: {
+    latitude: Number,
+    longitude: Number,
+    accuracy: Number,
+    timestamp: { type: Date, default: Date.now }
+  },
+  locationHistory: [
+    {
+      latitude: Number,
+      longitude: Number,
+      accuracy: Number,
+      timestamp: { type: Date, default: Date.now }
+    }
+  ],
   status: String,
   updatedAt: Date,
 });
